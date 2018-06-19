f237f793d0b1d6664be3e5b2048bd93941924f36
NatholBMX <snake-lizard@freenet.de>
2018-06-19 11:47:39 +0200

    Updated testing

--- testing/testing_cpm.py
+++ testing/testing_cpm.py
@@ -1,14 +1,22 @@
 from imageAnalysis import handTracking
 
 
-def test_init_cpm():
-    print("Before Init: ", handTracking.tf_session)
+def try_init_cpm():
+    print("Before Init: ")
+    print("Kalman Filter: ", handTracking.kalman_filter_array)
+    print("Output node: ", handTracking.output_node)
+    print("TF Session: ", handTracking.tf_session)
+    print("Tracker: ", handTracking.tracker)
     handTracking.init_cpm_session()
-    print("After Init: ", handTracking.tf_session)
+    print("After Init: ")
+    print("Kalman Filter: ", handTracking.kalman_filter_array)
+    print("Output node: ", handTracking.output_node)
+    print("TF Session: ", handTracking.tf_session)
+    print("Tracker: ", handTracking.tracker)
 
 
 def main():
-    test_init_cpm()
+    try_init_cpm()
 
 
 if __name__ == "__main__":
